0bfe1ad80855b88979d30732d85e1b4719c079d6
callnirajgupta <callnirajgupta@gmail.com>
2021-05-15 13:45:25 +0530

    updated

--- src/test/java/com/abhinavautomation/stepdef/GlobalStepDefinition.java
+++ src/test/java/com/abhinavautomation/stepdef/GlobalStepDefinition.java
@@ -60,9 +60,9 @@ public class GlobalStepDefinition {
 		}
 		if (SeleniumUtil.getDriver() != null) {
 			
-			//SeleniumUtil.closeBrowser();
-			//SeleniumUtil.setDriver(null);
-			//SeleniumUtil.driverStatus=false;
+			SeleniumUtil.closeBrowser();
+			SeleniumUtil.setDriver(null);
+			SeleniumUtil.driverStatus=false;
 		}
 
 		
